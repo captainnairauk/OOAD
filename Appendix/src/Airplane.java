cbe1d39792817aafe603019e0ccc87b81833366f
Aniket Kaimal <aniket1995@yahoo.co.in>
2023-06-25 18:11:43 +0530

    Update Appendix folder

---
+++ Appendix/src/Airplane.java
@@ -0,0 +1,15 @@
+public class Airplane {
+    public int speed;
+
+    public Airplane() {
+        this.speed = 0;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
